64019e9919cb87abc0822434c222621197137e44
aysha-02 <ayshasiddika2520@gmail.com>
2026-07-03 21:07:35 +0600

    Add files via upload

---
+++ dsa_lab/recurssion/summation.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int sum(int n)
+{
+    if (n == 0)
+        return 0;
+    else
+        return n + sum(n - 1);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Sum = %d", sum(n));
+
+    return 0;
+}
